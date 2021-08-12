777d3479715fb37e1be9e9943c99075e45b492c8
ikeg225 <ikeg225@berkeley.edu>
2021-08-11 17:05:01 -0700

    Basic Game Format

--- script.js
+++ script.js
@@ -1,61 +1,43 @@
-function computerPlay() {
+const buttons = document.querySelectorAll('button');
+
+function robotPlay() {
     const options = ['Rock', 'Paper', 'Scissors'];
     const random = Math.floor(Math.random() * options.length);
     return options[random]
-}
-
-function capitalize(word) {
-    return word[0].toUpperCase() + word.slice(1).toLowerCase()
-}
+};
 
 function playRound(input) {
-    let weapon = capitalize(input);
-    if (computerPlay() == weapon) {
-        return "It's a Tie!"
-    }else if (computerPlay() == "Rock") {
-        if (weapon == "Paper") {
-            return "You Win! Paper beats Rock."
+    robot = robotPlay();
+
+    if (robot == input) {
+        document.getElementById("winLose").innerHTML = "It's a Tie!"
+    } else if (robot == "Rock") {
+        if (input == "Paper") {
+            document.getElementById("winLose").innerHTML = "You Win! Paper beats Rock."
         }
         else {
-            return "You Lose! Rock beats Scissors."
+            document.getElementById("winLose").innerHTML = "You Lose! Rock beats Scissors."
         }
-    }else if (computerPlay() == "Paper") {
-        if (weapon == "Scissors"){
-            return "You Win! Scissors beats Rock."
-        }
-        else {
-            return "You Lose! Paper beats Rock."
-        }
-    }else {
-        if (weapon == "Rock"){
-            return "You Win! Rock beats Scissors."
+    } else if (robot == "Paper") {
+        if (input == "Scissors"){
+            document.getElementById("winLose").innerHTML = "You Win! Scissors beats Rock."
         }
         else {
-            return "You Lose! Scissors beats Paper."
+            document.getElementById("winLose").innerHTML = "You Lose! Paper beats Rock."
         }
-    }
-
-}
-
-function game() {
-    let botPoints = 0;
-    let playerPoints = 0;
-    for (let i = 1; i < 6; i++) {
-        let weapon = prompt("Choose your weapon: Rock, Paper, or Scissors! (Round " + i + ")");
-        let result = playRound(weapon);
-        if (result.includes("Win")) {
-            playerPoints++;
-        } else {
-            botPoints++;
-        }
-        console.log(result)
-
-    }
-    if (playerPoints > botPoints) {
-        console.log("Game Over - You Win!")
     } else {
-        console.log("Game Over - You Lose!")
+        if (input == "Rock"){
+            document.getElementById("winLose").innerHTML = "You Win! Rock beats Scissors."
+        }
+        else {
+            document.getElementById("winLose").innerHTML = "You Lose! Scissors beats Paper."
+        }
     }
-}
 
-game()
+};
+
+buttons.forEach((button) => {
+    button.addEventListener(('click'), () => {
+        playRound(button.textContent);
+    });
+});
