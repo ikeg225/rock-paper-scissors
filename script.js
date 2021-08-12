3e9966d8050ae2c3f48ba2e80da61e01c4a73edc
ikeg225 <ikeg225@berkeley.edu>
2021-08-12 09:13:49 -0700

    Reset Button

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let human = 0;
 let rbtScore = 0;
 let round = 0;
 
-const buttons = document.querySelectorAll('button');
+const op = document.querySelector('#options');
+const buttons = op.querySelectorAll('button');
 
 function robotPlay() {
     const options = ['Rock', 'Paper', 'Scissors'];
@@ -49,9 +50,14 @@ function playRound(input) {
 
 function update() {
     document.getElementById("round").innerHTML = "Round: " + round;
-    document.getElementById("winRate").innerHTML = "Win Rate: " + Math.round((human / (human + rbtScore)) * 100) + "%";
     document.getElementById("human").innerHTML = "Human: " + human;
     document.getElementById("robot").innerHTML = "Robot: " + rbtScore;
+
+    if (human + rbtScore != 0) {
+        document.getElementById("winRate").innerHTML = "Win Rate: " + Math.round((human / (human + rbtScore)) * 100) + "%";
+    } else {
+        document.getElementById("winRate").innerHTML = "Win Rate: 0%";
+    }
 }
 
 
@@ -60,4 +66,10 @@ buttons.forEach((button) => {
         playRound(button.textContent);
         update();
     });
+});
+
+document.querySelector('#reset').addEventListener(('click'), () => {
+    human = rbtScore = round = 0;
+    update();
+    document.getElementById("winLose").innerHTML = "Click a Button Above!";
 });
